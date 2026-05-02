be70ed490e2b9651a3d86dc73dd3ef12c69cceda
oll-soll <ollsollina@gmail.com>
2026-05-02 20:06:01 +0500

    fix: update the filtering function

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,36 +1,45 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName]).map(name => {
+                    const el = document.createElement('option');
+                    el.textContent = name;
+                    el.value = name;
+                    return el;
+                })
+            )
+        })
+    }
 
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                        // используйте name как значение и текстовое содержимое
-                        const option = document.createElement('option');
-                        option.value = name;
-                        option.textContent = name;
-                        return option;
-                    })
-        );
-    })
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    const applyFiltering = (query, state, action) => {
+
         if (action && action.name === 'clear') {
-            let input = action.parentElement.querySelector('input');
-
-            if (input) {
-                input.value = '';
-            }
-
             const field = action.dataset.field;
-            state[field] = '';
+            const input = action.parentElement.querySelector('input, select');
+
+            if (input) input.value = '';
+            if (field) state[field] = '';
         }
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state)); 
+        const filter = {};
+
+        Object.keys(elements).forEach(key => {
+            const el = elements[key];
+
+            if (el && ['INPUT', 'SELECT'].includes(el.tagName) && el.value) {
+                filter[`filter[${el.name}]`] = el.value;
+            }
+        });
+
+        return Object.keys(filter).length
+            ? Object.assign({}, query, filter)
+            : query;
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
--- src/main.js
+++ src/main.js
@@ -43,6 +43,7 @@ async function render(action) {
     // result = applySearching(result, state, action);
     // result = applyFiltering(result, state, action);
     // result = applySorting(result, state, action);
+    query = applyFiltering(query, state, action);
     query = applyPagination(query, state, action); // обновляем query
 
     const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
@@ -58,9 +59,7 @@ const sampleTable = initTable({
     after: ['pagination']
 }, render);
 // @todo: инициализация
-/*const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
-    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
-});*/
+const {applyFiltering, updateIndexes} = initFiltering(sampleTable.filter.elements);
 
 const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
     sampleTable.header.elements.sortByDate,
@@ -85,6 +84,10 @@ appRoot.appendChild(sampleTable.container);
 
 async function init() {
     const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+    });
 }
 
 init().then(render);
